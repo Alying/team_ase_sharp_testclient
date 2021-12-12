ff183027dd2323d0b904b763e6014546bbfa8a6b
Alina Ying <alina.ying@gmail.com>
2021-12-11 16:17:10 -0800

    adding WebClient; copied over from original code in Safe Travel Service

---
+++ WebClient/WebClient/Controllers/ValuesController.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Newtonsoft.Json;
+
+namespace WebClient.Controllers
+{
+    public class ClientResponse
+    {
+        [JsonProperty("state")]
+        public string State { get; set; }
+
+        [JsonProperty("recommendationState")]
+        public string RecommendationState { get; set; }
+
+        [JsonProperty("overallScore")]
+        public float OverallScore { get; set; }
+
+        [JsonProperty("airQualityScore")]
+        public float AirQualityScore { get; set; }
+
+        [JsonProperty("covidIndexScore")]
+        public float CovidIndexScore { get; set; }
+
+        [JsonProperty("weatherScore")]
+        public float WeatherScore { get; set; }
+    }
+
+    public class ValuesController : ApiController
+    {
+        private static async Task<List<ClientResponse>> RecommendationClient(string endpoint)
+        {
+            HttpClient client = new HttpClient();
+
+            var resp = await client.GetAsync(endpoint);
+            var stringValue = await resp.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<List<ClientResponse>>(stringValue);
+        }
+
+        private static async Task<ClientResponse> LocationInquiryClient(string endpoint)
+        {
+            HttpClient client = new HttpClient();
+
+            var resp = await client.GetAsync(endpoint);
+            var stringValue = await resp.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<ClientResponse>(stringValue);
+        }
+
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] {
+                "endpoint should be https://localhost:44324/api/values/1 for the Recommendation Client",
+                "endpoint should be https://localhost:44324/api/values/2 for the Location Inquiry Client"
+            };
+        }
+
+        // GET api/values/{id}
+        public async Task<string> Get(int id)
+        {
+            // can hit either recommendation or location inquiry endpoint from Safe Travel Service _once_
+            string recommendationEndpoint = "https://localhost:5001/api/recommendations";
+            string locationInquiryEndpoint = "https://localhost:5001/api/recommendations/country/US/state/CA";
+
+            if (id == 1)
+            {
+                System.Diagnostics.Debug.WriteLine(id);
+
+                string result = "";
+                var task = await RecommendationClient(recommendationEndpoint);
+                foreach (var clientResponse in task)
+                {
+                    result += clientResponse.State + " " + clientResponse.RecommendationState + " " + clientResponse.OverallScore + "\n";
+                }
+
+                return result;
+            }
+            else
+            {
+                var clientResponse = await LocationInquiryClient(locationInquiryEndpoint);
+                string result = clientResponse.CovidIndexScore + " " +
+                    clientResponse.AirQualityScore + " " +
+                    clientResponse.OverallScore + " " +
+                    clientResponse.RecommendationState;
+                return result;
+            }
+        }
+
+        // POST api/values. Not used.
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5. Not used.
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5. Not used.
+        public void Delete(int id)
+        {
+        }
+    }
+}
