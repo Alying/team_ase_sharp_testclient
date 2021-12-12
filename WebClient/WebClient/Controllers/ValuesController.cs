b50f8450280a29915b7e14c626933e8323daa8a5
Alina Ying <alina.ying@gmail.com>
2021-12-11 18:57:54 -0800

    cleaned up code a little bit

--- WebClient/WebClient/Controllers/ValuesController.cs
+++ WebClient/WebClient/Controllers/ValuesController.cs
@@ -32,19 +32,17 @@ namespace WebClient.Controllers
         private static async Task<List<ClientResponse>> RecommendationClient(string endpoint)
         {
             HttpClient client = new HttpClient();
-
-            var resp = await client.GetAsync(endpoint);
-            var stringValue = await resp.Content.ReadAsStringAsync();
-            return JsonConvert.DeserializeObject<List<ClientResponse>>(stringValue);
+            HttpResponseMessage response = await client.GetAsync(endpoint);
+            string responseValue = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<List<ClientResponse>>(responseValue);
         }
 
         private static async Task<ClientResponse> LocationInquiryClient(string endpoint)
         {
             HttpClient client = new HttpClient();
-
-            var resp = await client.GetAsync(endpoint);
-            var stringValue = await resp.Content.ReadAsStringAsync();
-            return JsonConvert.DeserializeObject<ClientResponse>(stringValue);
+            HttpResponseMessage response = await client.GetAsync(endpoint);
+            string responseValue = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<ClientResponse>(responseValue);
         }
 
         // GET api/values
@@ -52,7 +50,7 @@ namespace WebClient.Controllers
         {
             return new string[] {
                 "endpoint should be https://localhost:44324/api/values/1 for the Recommendation Client",
-                "endpoint should be https://localhost:44324/api/values/2 for the Location Inquiry Client"
+                "endpoint should be https://localhost:44324/api/values/2 for the Location Inquiry Client",
             };
         }
 
@@ -63,15 +61,18 @@ namespace WebClient.Controllers
             string recommendationEndpoint = "https://localhost:5001/api/recommendations";
             string locationInquiryEndpoint = "https://localhost:5001/api/recommendations/country/US/state/CA";
 
+            System.Diagnostics.Debug.WriteLine(id);
+
             if (id == 1)
             {
-                System.Diagnostics.Debug.WriteLine(id);
 
                 string result = "";
                 var task = await RecommendationClient(recommendationEndpoint);
                 foreach (var clientResponse in task)
                 {
-                    result += clientResponse.State + " " + clientResponse.RecommendationState + " " + clientResponse.OverallScore + "\n";
+                    result += clientResponse.State +
+                        " " + clientResponse.RecommendationState +
+                        " " + clientResponse.OverallScore + "\n";
                 }
 
                 return result;
@@ -79,11 +80,11 @@ namespace WebClient.Controllers
             else
             {
                 var clientResponse = await LocationInquiryClient(locationInquiryEndpoint);
-                string result = clientResponse.CovidIndexScore + " " +
-                    clientResponse.AirQualityScore + " " +
-                    clientResponse.OverallScore + " " +
-                    clientResponse.RecommendationState;
-                return result;
+                return clientResponse.CovidIndexScore +
+                    " " + clientResponse.AirQualityScore +
+                    " " + clientResponse.OverallScore +
+                    " " + clientResponse.RecommendationState;
+
             }
         }
 
