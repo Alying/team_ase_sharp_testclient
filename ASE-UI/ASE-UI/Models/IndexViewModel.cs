2578cb365de10538a7a62a0b13cd4f50bda12c58
Muzhi Yang <muzhiyang@microsoft.com>
2021-12-15 22:23:18 -0800

    new version

---
+++ ASE-UI/ASE-UI/Models/IndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASE_UI.Models
+{
+    public class IndexViewModel
+    {
+        public string Result { get; set; }
+    }
+}
