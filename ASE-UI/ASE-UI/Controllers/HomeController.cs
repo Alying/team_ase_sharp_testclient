0e3ddaba5ae2323e78cd8406d17dd6e0eb757e39
Alan Yang <yangmuzhi1993@gmail.com>
2021-12-16 15:52:53 -0800

    Finish oauth2

--- ASE-UI/ASE-UI/Controllers/HomeController.cs
+++ ASE-UI/ASE-UI/Controllers/HomeController.cs
@@ -6,12 +6,16 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ASE_UI.Controllers
 {
     public class HomeController : Controller
     {
+        private const string _client_id = "1091126786905-9qetr8clasin3ph65blhn9jut088c1kf.apps.googleusercontent.com";
+        private const string _client_secret = "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";
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -24,23 +28,68 @@ namespace ASE_UI.Controllers
             return View();
         }
 
+        public IActionResult LogOut()
+        {
+            return View("Index", new IndexViewModel());
+        }
+
+        public IActionResult Auth()
+        {
+            var url = $"https://accounts.google.com/o/oauth2/auth?client_id={_client_id}&nonce=123&scope=https://www.googleapis.com/auth/userinfo.email openid&response_type=code&redirect_uri=https://lvh.me:5002/Home/ExchangeToken";
+            return Redirect(url);
+        }
+
+        public async Task<IActionResult> ExchangeToken()
+        {
+            var queries = Request.Query;
+            if (!queries.ContainsKey("code")) 
+            {
+                return View();
+            }
+
+            var code = queries["code"].ToString();
+            var client = new RestClient("https://accounts.google.com");
+            var request = new RestRequest("o/oauth2/token", Method.POST);
+
+            request.AddParameter("code", code);
+            request.AddParameter("redirect_uri", "https://lvh.me:5002/Home/ExchangeToken");
+            request.AddParameter("client_id", _client_id);
+            request.AddParameter("client_secret", _client_secret);
+            request.AddParameter("grant_type", "authorization_code");
+
+            var result = await client.ExecuteAsync<TokenResponse>(request);
+            
+            HttpContext.Session.Set("code", Encoding.ASCII.GetBytes(result.Data.access_token));
+            await HttpContext.Session.CommitAsync();
+
+            return View("Index", new IndexViewModel { Token = result.Data.access_token });
+        }
+
         public async Task<IActionResult> GetRecommendation(string countryCode, string state) 
         {
+             await HttpContext.Session.LoadAsync();
+            var token = HttpContext.Session.TryGetValue("code", out var byteValue);
+
+            var model = new IndexViewModel { Token = Encoding.ASCII.GetString(byteValue) };
+
             if (string.IsNullOrEmpty(countryCode) && string.IsNullOrEmpty(state))
             {
 
                 var defaultResult = await GetDefaultRecommendationAsync();
 
-                return View("Index", new IndexViewModel { Result = defaultResult });
+                model.Result = defaultResult;
+                return View("Index", model);
             }
 
             if (!string.IsNullOrEmpty(countryCode) && !string.IsNullOrEmpty(state)) 
             {
                 var result = await GetSpecificRecommendationAsync(countryCode, state);
-                return View("Index", new IndexViewModel { Result = result });
+                model.Result = result;
+                return View("Index", model);
             }
 
-            return View("Index", new IndexViewModel { Result = "CountryCode and State must be both provided or both empty." });
+            model.Result = "CountryCode and State must be both provided or both empty.";
+            return View("Index", model);
         }
 
         public IActionResult Privacy()
---
+++ ASE-UI/ASE-UI/Models/TokenResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace ASE_UI.Models
+{
+    public class TokenResponse
+    {
+        public string access_token { get; set; }
+
+        public string token_type { get; set; }
+
+        public string id_token { get; set; }
+    }
+}
